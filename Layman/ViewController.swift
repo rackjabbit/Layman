348181aa88dab098b89200008bff0f53a9b17056
Hans Belka <hans.belka40@gmail.com>
2022-03-04 23:23:56 -0700

    Section 2.3, added show message button

--- Layman/ViewController.swift
+++ Layman/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("😅 View did load has run")
+        messageLabel.text = "Wow!!!"
     }
 
-
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+        print("😅 The message button was pressed!?!?")
+        messageLabel.text = "Get 'er done"
+    }
+    
 }
 
